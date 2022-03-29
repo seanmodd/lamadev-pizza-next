b9f554ba6af01ff266745ed554e4e458bcd9656e
Sean <seansmodd@gmail.com>
2022-03-29 11:52:58 -0700

    trying to fix what i can

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-import Add from "../components/Add";
-import AddButton from "../components/AddButton";
-import Featured from "../components/Featured";
-import PizzaList from "../components/PizzaList";
-import styles from "../styles/Home.module.css";
+import axios from 'axios';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+import Add from '../components/Add';
+import AddButton from '../components/AddButton';
+import Featured from '../components/Featured';
+import PizzaList from '../components/PizzaList';
+import styles from '../styles/Home.module.css';
 
 export default function Home({ pizzaList, admin }) {
   const [close, setClose] = useState(true);
@@ -18,26 +18,31 @@ export default function Home({ pizzaList, admin }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      {<AddButton setClose={setClose} />}
-      <PizzaList pizzaList={pizzaList} />
+      <AddButton setClose={setClose} />
+      {/* <PizzaList pizzaList={pizzaList} /> */}
       {!close && <Add setClose={setClose} />}
     </div>
   );
 }
 
 export const getServerSideProps = async (ctx) => {
-  const myCookie = ctx.req?.cookies || "";
+  const myCookie = ctx.req?.cookies || '';
   let admin = false;
-
-  if (myCookie.token === process.env.TOKEN) {
+  // if (myCookie.token === process.env.TOKEN) {
+  if (myCookie.__utma === process.env.TOKEN) {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  console.log('This is process.env.TOKEN: ', process.env.TOKEN);
+  console.log('This is myCookie.token: ', myCookie.token);
+  console.log('This is myCookie.__utma: ', myCookie.__utma);
+  console.log('This is ctx.req?.cookies: ', ctx.req?.cookies);
+  // const res = await axios.get('http://localhost:3000/api/products');
+  // console.log('This is res: ', res);
   return {
     props: {
-      pizzaList: res.data,
-      admin,
+      // pizzaList: res.data,
+      // admin,
     },
   };
 };
