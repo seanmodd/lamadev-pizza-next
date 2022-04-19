31a60ed9fd500eb2456c4e048b8c2c2f7706c450
Sean <seansmodd@gmail.com>
2022-04-19 12:18:10 -0700

    idk

--- pages/index.js
+++ pages/index.js
@@ -26,12 +26,12 @@ export default function Home({ pizzaList, admin }) {
 }
 
 export const getServerSideProps = async (ctx) => {
-  // const myCookie = ctx.req?.cookies || '';
-  // let admin = false;
+  const myCookie = ctx.req?.cookies || '';
+  let admin = false;
 
-  // if (myCookie.token === process.env.TOKEN) {
-  //   admin = true;
-  // }
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
 
   // const res = await axios.get('http://localhost:3000/api/products');
   const data = [];
@@ -56,7 +56,7 @@ export const getServerSideProps = async (ctx) => {
   return {
     props: {
       pizzaList: data,
-      // admin,
+      admin,
     },
   };
 };
